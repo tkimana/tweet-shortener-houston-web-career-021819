4ceef96f0237e7bc43064fe9855bcb3d79d33aa9
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-17 19:17:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,2 +1,3 @@
-def word_substituer
+def tweet_shortener
+  puts {"Hi guys, can any 1 teach me how 2 b cool? I really want 2 b the best @ everything, you know what I mean? Tweeting is super fun guys!!!"}
   
